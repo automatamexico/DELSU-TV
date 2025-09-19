7ba44988beb3467d24486bf84c0187a0bddf37b8
automatamexico <desarrollo.solucionesdelsu@gmail.com>
2025-09-18 21:25:31 -0600

    Create supabaseClient.js

---
+++ src/supabaseClient.js
@@ -0,0 +1,4 @@
+// src/supabaseClient.js
+// Puente para que cualquier import '../supabaseClient' funcione.
+// Reexporta el cliente que ya tienes en context/supabaseClients.
+export { supabase } from './context/supabaseClients';
