78fd61c9d25a211bbf5f1158286b5dd00adf8e09
automatamexico <desarrollo.solucionesdelsu@gmail.com>
2025-10-01 18:32:59 -0600

    Update supabaseclients.js

--- src/supabaseclients.js
+++ src/supabaseclients.js
@@ -1,8 +1,6 @@
 // src/supabaseclients.js
-import { supabase as client } from './supabaseClients';
+import supabaseDefault, { supabase } from './supabaseClientCore';
+export { supabase };
+export default supabaseDefault;
 
-// Soporta import nombrado y por defecto:
-// import { supabase } from '../supabaseclients'
-// import supabase from '../supabaseclients'
-export const supabase = client;
-export default client;
+
