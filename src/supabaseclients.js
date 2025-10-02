fd5305aaedb5f2f3aa4457d67a3484fdd710f1ea
automatamexico <desarrollo.solucionesdelsu@gmail.com>
2025-10-01 18:48:57 -0600

    Update supabaseclients.js

--- src/supabaseclients.js
+++ src/supabaseclients.js
@@ -1,6 +1,5 @@
-// src/supabaseclients.js
-import supabaseDefault, { supabase } from './supabaseClientCore';
-export { supabase };
-export default supabaseDefault;
+// src/supabaseClients.js
+export { supabase } from './supabaseClientCore';
+export { default } from './supabaseClientCore';
 
 
