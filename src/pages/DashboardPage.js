05620e0c6127e393be7cf271188d3a04e86c14e2
automatamexico <desarrollo.solucionesdelsu@gmail.com>
2026-01-16 21:27:47 -0600

    Update DashboardPage.js

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -233,7 +233,7 @@ export default function DashboardPage() {
                 {/* Altura ajustada para que la leyenda quede visible sin empujar el layout */}
                 <ChannelGeoMap
                   channelId={channels[0].id}
-                  className="h-[360px] md:h-[400px] lg:h-[420px]"
+                  className="h-[500px] md:h-[540px] lg:h-[560px]"
                 />
               </div>
             </div>
