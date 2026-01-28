a5fa96da6cc90642c20bff2569b7d3fbf80cad5d
automatamexico <desarrollo.solucionesdelsu@gmail.com>
2026-01-27 19:20:57 -0600

    Update ChannelCard.js

--- src/components/ChannelCard.js
+++ src/components/ChannelCard.js
@@ -242,7 +242,7 @@ export default function ChannelCard({ channel, onClick }) {
                       alt="Android"
                       loading="lazy"
                       decoding="async"
-                      className="h-14 w-14 object-contain"
+                      className="h-17 w-17 object-contain"
                     />
                   </button>
                 </div>
